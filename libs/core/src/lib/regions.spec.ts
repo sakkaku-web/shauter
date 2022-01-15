1de5609149ceca3393d6ae62019730394bed0f43
myin <minyin142p@gmail.com>
2022-01-15 11:57:25 +0100

    feat: get regions for box

--- libs/core/src/lib/regions.spec.ts
+++ libs/core/src/lib/regions.spec.ts
@@ -1,22 +1,89 @@
 import { RegionService } from './regions';
 
 describe('RegionService', () => {
-  test.each`
-    point                    | region
-    ${{ lat: 0, long: 0 }}   | ${{ lat: 0, long: 0 }}
-    ${{ lat: 0.5, long: 0 }} | ${{ lat: 0, long: 0 }}
-    ${{ lat: 1, long: 0 }}   | ${{ lat: 1, long: 0 }}
-    ${{ lat: 1.5, long: 0 }} | ${{ lat: 1, long: 0 }}
-    ${{ lat: 0, long: 0.5 }} | ${{ lat: 0, long: 0 }}
-    ${{ lat: 0, long: 1 }}   | ${{ lat: 0, long: 1 }}
-    ${{ lat: 0, long: 1.5 }} | ${{ lat: 0, long: 1 }}
-  `('get region for point ($point.lat, $point.long)', ({ point, region }) => {
+  describe('GetRegionForPoint', () => {
+    test.each`
+      point                      | region
+      ${{ lat: 0, long: 0 }}     | ${{ lat: 0, long: 0 }}
+      ${{ lat: 0.5, long: 0 }}   | ${{ lat: 0, long: 0 }}
+      ${{ lat: 1, long: 0 }}     | ${{ lat: 1, long: 0 }}
+      ${{ lat: 1.5, long: 0 }}   | ${{ lat: 1, long: 0 }}
+      ${{ lat: 0, long: 0.5 }}   | ${{ lat: 0, long: 0 }}
+      ${{ lat: 0, long: 1 }}     | ${{ lat: 0, long: 1 }}
+      ${{ lat: 0, long: 1.5 }}   | ${{ lat: 0, long: 1 }}
+      ${{ lat: 1.5, long: 1.5 }} | ${{ lat: 1, long: 1 }}
+    `(
+      'get region for simple point ($point.lat, $point.long)',
+      ({ point, region }) => {
+        const service = new RegionService({
+          lat: 10,
+          long: 10,
+          latDividor: 10,
+          longDividor: 10,
+        });
+        expect(service.getRegionForPoint(point)).toEqual(region);
+      }
+    );
+
+    test.each`
+      point                      | region
+      ${{ lat: 0, long: 0 }}     | ${{ lat: 0, long: 0 }}
+      ${{ lat: 0.01, long: 0 }}  | ${{ lat: 0, long: 0 }}
+      ${{ lat: 0.5, long: 0.5 }} | ${{ lat: 5, long: 5 }}
+      ${{ lat: 0.1, long: 1 }}   | ${{ lat: 1, long: 10 }}
+    `(
+      'get region with decimal dividor for point ($point.lat, $point.long)',
+      ({ point, region }) => {
+        const service = new RegionService({
+          lat: 10,
+          long: 10,
+          latDividor: 100,
+          longDividor: 100,
+        });
+        expect(service.getRegionForPoint(point)).toEqual(region);
+      }
+    );
+  });
+
+  describe('GetRegionsForBox', () => {
     const service = new RegionService({
       lat: 10,
       long: 10,
       latDividor: 10,
       longDividor: 10,
     });
-    expect(service.getRegionForPoint(point)).toEqual(region);
+
+    test('get for box', () => {
+      const regions = service.getRegionsForBox({
+        leftTop: { lat: 1, long: -1 },
+        leftBottom: { lat: -1, long: -1 },
+        rightTop: { lat: 1, long: 1 },
+        rightBottom: { lat: -1, long: 1 },
+      });
+
+      expect(regions).toEqual(
+        expect.arrayContaining([
+          { lat: -1, long: -1 },
+          { lat: -1, long: 0 },
+          { lat: -1, long: 1 },
+          { lat: 0, long: -1 },
+          { lat: 0, long: 0 },
+          { lat: 0, long: 1 },
+          { lat: 1, long: -1 },
+          { lat: 1, long: 0 },
+          { lat: 1, long: 1 },
+        ])
+      );
+    });
+
+    test('get for invalid box', () => {
+      const regions = service.getRegionsForBox({
+        leftTop: { lat: 10, long: 10 },
+        leftBottom: { lat: 10, long: 10 },
+        rightTop: { lat: 5, long: 5 },
+        rightBottom: { lat: 5, long: 5 },
+      });
+      expect(regions).toEqual([]);
+    });
   });
 });
--- libs/core/src/lib/regions.ts
+++ libs/core/src/lib/regions.ts
@@ -1,10 +1,17 @@
-import { Coordinate } from '..';
+import { Coordinate } from './core';
 
 interface RegionServiceOptions {
   lat: number;
   long: number;
   latDividor: number;
   longDividor: number;
+}
+
+interface RegionBox {
+  leftTop: Coordinate;
+  leftBottom: Coordinate;
+  rightTop: Coordinate;
+  rightBottom: Coordinate;
 }
 
 export class RegionService {
@@ -23,5 +30,44 @@ export class RegionService {
     const long = Math.floor(coor.long / dividor.long);
 
     return { lat, long };
+  }
+
+  private isValidBox({
+    leftBottom,
+    leftTop,
+    rightBottom,
+    rightTop,
+  }: RegionBox): boolean {
+    return (
+      leftTop.lat > leftBottom.lat &&
+      leftTop.long < rightTop.long &&
+      rightBottom.long > leftBottom.long &&
+      rightBottom.lat < rightTop.lat &&
+      leftTop.lat > rightBottom.lat &&
+      leftTop.long < rightBottom.long
+    );
+  }
+
+  getRegionsForBox(box: RegionBox): Coordinate[] {
+    if (!box || !this.isValidBox(box)) {
+      return [];
+    }
+
+    const start = box.leftBottom;
+    const end = box.rightTop;
+
+    const diff: Coordinate = {
+      lat: end.lat - start.lat,
+      long: end.long - start.long,
+    };
+
+    const result: Coordinate[] = [];
+    for (let i = 0; i <= diff.lat; i++) {
+      for (let j = 0; j <= diff.long; j++) {
+        result.push({ lat: start.lat + i, long: start.long + j });
+      }
+    }
+
+    return result;
   }
 }
