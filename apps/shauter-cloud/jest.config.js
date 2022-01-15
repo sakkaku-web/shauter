830a4b47c78201701f5a4997d846ececeaccc2aa
myin <minyin142p@gmail.com>
2022-01-15 14:15:21 +0100

    chore: rename shauter-cloud

--- apps/shauter-cloud/jest.config.js
+++ apps/shauter-cloud/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'aws-cloud',
+  displayName: 'shauter-cloud',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/aws-cloud',
+  coverageDirectory: '../../coverage/apps/shauter-cloud',
 };
--- apps/shauter-cloud/src/main.ts
+++ apps/shauter-cloud/src/main.ts
@@ -2,4 +2,4 @@ import * as cdk from '@aws-cdk/core';
 import { AppStack } from './stacks/app-stack';
 
 const app = new cdk.App();
-new AppStack(app, 'aws-cloud');
+new AppStack(app, 'shauter-cloud');
